90f7b4aa830095f37e8ff1437793f4f9cc94cef7
Assane CISSE <a.cisse@groupeonepoint.com>
2022-05-19 22:40:16 +0200

    add main file

---
+++ test_main.py
@@ -0,0 +1,45 @@
+
+import logging
+import sys
+import unittest
+
+logger = logging.getLogger()
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(
+    logging.Formatter(
+        '[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d %(message)s'))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+if __name__ == '__main__':
+
+
+  # Run the tests
+  if ((len(sys.argv) < 2) or (sys.argv[1] == '?') or (sys.argv[1] == 'help')):
+    print ""
+    print ""
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    print "----- HELP :"
+    print "----- python test_RED.py <Type of test> <number of test>"
+    print "       or"
+    print "----- python test_RED.py <Type of test>"
+    print ""
+    print "----- Example: python test_RED.py REG 000 / python test_RED.py REG "
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    print ""
+
+
+  else :    
+    if (len(sys.argv) == 2):
+      filename = 'RED_FT_S_' + sys.argv[1] + '_*' + '.py'
+    else:
+      filename = 'RED_FT_S_' + sys.argv[1] + '_' + str(sys.argv[2]) + '.py'
+    #end_if
+      
+    print filename
+
+    tests = unittest.TestLoader().discover('testcases', filename)
+    unittest.TextTestRunner(verbosity=0).run(tests)
+
+  #end_if
+  
