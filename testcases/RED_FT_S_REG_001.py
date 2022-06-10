b6d5c47830b277935fdf62b33ebdbba6798c1072
Assane CISSE <assane.cisse3@gmail.com>
2022-06-10 15:02:46 +0200

    Clean code REG file

--- testcases/RED_FT_S_REG_001.py
+++ testcases/RED_FT_S_REG_001.py
@@ -1,6 +1,4 @@
 # registration
-
-from datetime import datetime
 import json
 import os
 import unittest
@@ -13,35 +11,28 @@ import unittest
 
 
 class RegistrationTestcase(unittest.TestCase):
-  def test_RED_FT_S_REG_001(self):
+    def test_RED_FT_S_REG_001(self):
 
-    print("---- BEGIN RED_FT_S_REG_001 ----")
-    # Load Devices
-    cbsd_a = json.load(
-        open(os.path.join('testcases', 'testdata', 'device_a.json')))
-    cbsd_b = json.load(
-        open(os.path.join('testcases', 'testdata', 'device_b.json')))
-    cbsd_c = json.load(
-        open(os.path.join('testcases', 'testdata', 'device_c.json')))
-    cbsd_d = json.load(
-        open(os.path.join('testcases', 'testdata', 'device_d.json')))
-    cbsd_list = [cbsd_a,cbsd_b,cbsd_c,cbsd_d]
-    # Inject FCC ID and User ID
-    for cbsd in cbsd_list:
-      print (cbsd)
-      self.assertTrue('cbsdSerialNumber' in cbsd)
-      self.assertTrue('longitude' in cbsd['installationParam'])
-      self.assertTrue('latitude' in cbsd['installationParam'])
-      self.assertFalse('cbsdId' in cbsd['installationParam'])
-      self.assertEqual(cbsd['airInterface']['radioTechnology'], 'E_UTRA')
-    print("---- END RED_FT_S_REG_001 SUCCESSFULL ----")
-##
-##
-
-
-
-
-
-
-
-	
+        print("---- BEGIN RED_FT_S_REG_001 ----")
+        # Load Devices
+        cbsd_a = json.load(
+            open(os.path.join('testcases', 'testdata', 'device_a.json')))
+        cbsd_b = json.load(
+            open(os.path.join('testcases', 'testdata', 'device_b.json')))
+        cbsd_c = json.load(
+            open(os.path.join('testcases', 'testdata', 'device_c.json')))
+        cbsd_d = json.load(
+            open(os.path.join('testcases', 'testdata', 'device_d.json')))
+        cbsd_list = [cbsd_a,cbsd_b,cbsd_c,cbsd_d]
+        # Inject FCC ID and User ID
+        for cbsd in cbsd_list:
+            print (cbsd)
+            print (cbsd['installationParam']['indoorDeployment'])
+            self.assertTrue('cbsdSerialNumber' in cbsd)
+            self.assertTrue('longitude' in cbsd['installationParam'])
+            self.assertTrue('latitude' in cbsd['installationParam'])
+            self.assertFalse('cbsdId' in cbsd['installationParam'])
+            self.assertEqual(cbsd['airInterface']['radioTechnology'], 'E_UTRA')
+        print("---- END RED_FT_S_REG_001 SUCCESSFULL ----")
+    ##
+    ##
